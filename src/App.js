fc0c7f9ad146cd4569847757b57388c19958220e
Ivan Okello <ivandamieno99@gmail.com>
2022-11-01 00:42:33 +0300

    Learned using the state useEffects use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
+import { useState } from 'react';
+
 import "./App.css";
 
-const Person=(props)=>{
-  return (
-    <>
-      <h1>Name: { props.name}</h1>
-      <h2>Last Name: { props.lastName}</h2>
-      <h2>Age: { props.age}</h2>
-    </>
+// Use of props and components
+// const Person=(props)=>{
+//   return (
+//     <>
+//       <h1>Name: { props.name}</h1>
+//       <h2>Last Name: { props.lastName}</h2>
+//       <h2>Age: { props.age}</h2>
+//     </>
 
-  )
-}
+//   )
+// }
 
 const App = () => {
+  const [counter,setCounter] = useState(0);
+
   return (
     <div className="App">
-      <Person name={'John'} lastName={'Doe'} age={30} />
+      {/* <Person name={'John'} lastName={'Doe'} age={30} />
       <Person name="Peter" lastName="Maina" age={ 75} />
       <Person/>
-    <h1>Hello, React</h1>
+    <h1>Hello, React</h1> */}
+      
+
+      <button onClick={() => setCounter((prevCount) =>prevCount -1)}>-</button>
+      <h1>{ counter}</h1>
+      <button onClick={() => setCounter((prevCount) =>prevCount +1)}>+</button>
+
   </div>
   );
 };
